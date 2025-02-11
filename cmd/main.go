01851148c90c7ce46c613d633c76751991ff75f1
Jhonata Nazareno <jhonatask@gmail.com>
2025-02-10 23:46:48 -0300

    Ajustado main goi

--- cmd/main.go
+++ cmd/main.go
@@ -13,20 +13,15 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
-
     // Migrar a entidade Agenda
     db.AutoMigrate(&entity.Agenda{})
-
     r := router.SetupRouter(db)
-
     // Configurar proxies confiáveis
     r.SetTrustedProxies([]string{"127.0.0.1"})
-
     // Definir a porta a partir da variável de ambiente ou usar a porta padrão 8080
     port := os.Getenv("PORT")
     if port == "" {
         port = "8080"
     }
-
     r.Run(":" + port)
 }
